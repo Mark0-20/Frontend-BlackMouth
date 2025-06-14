41763825660a736317c17ca015d1be0888a3c870
Miguel Munoz Hernandez <mmh79381@lasallebajio.edu.mx>
2025-06-13 20:08:04 -0600

    Initial Commit

---
+++ BlackmouthFront/BlackmouthFrontApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackmouthFrontApp.swift
+//  BlackmouthFront
+//
+//  Created by Jakeline Hernandez reynoso on 13/06/25.
+//
+
+import SwiftUI
+
+@main
+struct BlackmouthFrontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlackmouthFrontTests/BlackmouthFrontTests.swift
@@ -0,0 +1,17 @@
+//
+//  BlackmouthFrontTests.swift
+//  BlackmouthFrontTests
+//
+//  Created by Jakeline Hernandez reynoso on 13/06/25.
+//
+
+import Testing
+@testable import BlackmouthFront
+
+struct BlackmouthFrontTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
